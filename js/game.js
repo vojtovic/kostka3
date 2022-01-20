e77340b257a7553b37273f9cd5eb673d047c5c31
vojtovic <78141571+vojtovic@users.noreply.github.com>
2022-01-20 09:36:08 +0100

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,70 +1,90 @@
-var hody = [];
-var timer = false;
-var h = 0;
+const cube = document.getElementById('cube');
+const play = document.getElementById('game');
+const result = document.getElementById('result');
+
+let hod = 0;
+let hody = [];
+let timer = false;
 
 
-
-function suma(cisla) {
-    var sum = 0;
-    cisla.forEach(function (value, index) {
-        sum += value;
-    })
-    return sum;
+function animace() {
+    hod = (Math.ceil(Math.random() * 6));
+    cube.src = `img/kostka${hod}.png`;
 }
 
-function maximum(cisla) {
-    var max = 1;
-    cisla.forEach(function (value, index) {
-        if (value > max) max = value;
-    })
-    return max;
+play.addEventListener('click', () => {
+
+    if (!timer) {
+        timer = setInterval(animace, 55);
+        play.innerHTML = 'STOP';
+    } else {
+        clearInterval(timer);
+        timer = false;
+        play.innerHTML = 'HOD';
+        hody.push(hod);
+        console.log(hody);
+        statistika();
+        min();
+        max();
+    }
+
+})
+
+result.addEventListener('click', () => {
+
+    hody.push(hod);
+    console.log(hody);
+    statistika();
+    min();
+    max();
+})
+
+
+
+
+function Stop() {
+    cube.src = `img/kostka${hod}.png`;
+    hody.push(hod);
+    console.log(hody);
+    statistika();
+    min();
+    max();
+    console.log("Stopped");
+    mixBut.removeEventListener("click", Stop);
+    mixBut.addEventListener("click", Start);
+    mixBut.value = "Start";
 }
 
-function minimum(cisla) {
-    var min = 6;
-    cisla.forEach(function (value, index) {
+function suma() {
+    let vysledek = 0;
+    hody.forEach((value) => {
+        vysledek += value;
+    });
+    return vysledek;
+}
+
+function min() {
+    let min = 6;
+    hody.forEach((value) => {
         if (value < min) min = value;
-    })
+    });
     return min;
 }
 
-function average(sum, count) {
-    return (sum / count).toFixed(2);
-}
-function animace() {
-    var h = Math.ceil(Math.random() * 6);
-    document.getElementById('cube').src = 'img/kostka' + h + '.png';
-
-}
-function vypis() {
-    document.getElementById('result').innerHTML = '<p>Hod: ' + h + '</p>';
-            document.getElementById('result').innerHTML +=
-                '<p>Počet hodů: ' + hody.length + '</p>';
-            document.getElementById('result').innerHTML +=
-                '<p>Součet hodů: ' + suma(hody) + '</p>';
-            document.getElementById('result').innerHTML +=
-                '<p>Průměr hodů: ' + average(suma(hody), hody.length) + '</p>';
-            document.getElementById('result').innerHTML +=
-                '<p>Nejvyšší hod: ' + maximum(hody) + '</p>';
-            document.getElementById('result').innerHTML +=
-                '<p>Nejvyšší hod: ' + minimum(hody) + '</p>';
-            document.getElementById('game').innerText = 'START';
-            return h;
+function max() {
+    let max = 1;
+    hody.forEach((value) => {
+        if (value > max) max = value;
+    });
+    return max;
 }
 
-
-
-document.getElementById('game').addEventListener('click',
-    function () {
-        if (!timer) {
-            timer = setInterval(animace, 40);
-            document.getElementById('game').innerText = 'STOP';
-
-        } else {
-            clearInterval(timer);
-            timer = false;
-            vypis();
-            maximum();
-            minimum();
-        }
-    })
+function statistika() {
+    result.innerHTML = `<br>`;
+    result.innerHTML = `<p>Aktuální hod: ${hod}</p>`;
+    result.innerHTML += `<p>Všechny hody: ${hody}</p>`;
+    result.innerHTML += `<p>Součet hodů: ${suma()}</p>`;
+    result.innerHTML += `<p>Průměr hodů: ${(suma() / hody.length).toFixed(2)}</p>`;
+    result.innerHTML += `<p>Minimální hod: ${min()}</p>`;
+    result.innerHTML += `<p>Maximální hod: ${max()}</p>`;
+}
